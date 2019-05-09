8baa68890a55976da29eb173c31c1d4ee224ea91
aks <kiyotaka.aks@gmail.com>
2019-05-09 12:01:16 +0900

    Add Sample Project

---
+++ src/samples/skeleton/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 2019/05/09
+ * Copyright ? 2019 Kiyotaka Akasaka. All rights reserved.
+ */ 
+
+#include <ai_mini4wd.h>
+
+int main(void)
+{
+	aiMini4wdInitialize(0);
+
+    while (1) {
+    }
+}
