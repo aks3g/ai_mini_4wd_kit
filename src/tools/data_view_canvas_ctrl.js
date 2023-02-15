654239856c3a892150f1d1d2eac9ff59909a8b44
aks3g <kiyotaka.aks@gmail.com>
2023-02-16 05:17:35 +0900

    add new tool to visualize sensor data."

---
+++ src/tools/data_view_canvas_ctrl.js
@@ -0,0 +1,307 @@
+﻿/*
+ * strategy_editor_canvas_ctrl.js
+ *
+ * Created: 2020/02/24
+ * Copyright 2019 Kiyotaka Akasaka. All rights reserved.
+ */
+/*-----------------------------------------------------------------------------
+ *J グラフのラベル
+ */
+var TrainigSensorName   = ["Position", "Policy", "Updated Policy", "Velocity", "Accel-X", "Accel-Y", "Accel-Z", "Pitch", "Roll", "Yaw", "rpm", "V-BATT", "I-MOT"];
+var TrainingSensorUnit  = ["-",        "km/h",     "km/h",     "km/h", "mG", "mG", "mG", "mdegree/sec", "mdegree/sec", "mdegree/sec", "rpm", "mV", "mA"];
+
+/*-----------------------------------------------------------------------------
+ *J グラフ描画に必要な情報
+ */
+var GraphTypeLine = 0;
+var GraphTypeBar  = 1;
+
+var LineColors = ["maroon",    "darkblue",   "darkgreen",  "indigo",    "crimson",   "darkcyan" ]
+var LineShadow = ["indianred", "dodgerblue", "aquamarine", "slateblue", "lightpink", "turquoise"]
+
+var FeatureColorTable = ["#000000", "#ff0000", "#191970", "#0000cd", "#00bfff", "#2f4f4f", "#228b22", "#7fffd4", "#000000"]
+
+
+/*-----------------------------------------------------------------------------
+ *J グラフ描画に必要な情報
+ */
+var FeatureColorTable = ["#ffffff", "#ff0000", "#8a2be2", "#0000cd", "#00bfff", "#adff2f", "#228b22", "#7fffd4", "#ffffff"]
+
+/*-----------------------------------------------------------------------------
+ *
+ */
+//J XY座標を表す構造体的なもの
+Position = function(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+UsageGuide = function (name, color, shadow) {
+  this.name = name;
+  this.color = color;
+  this.shadow = shadow;
+}
+
+// キャンパス(w x h)内で、原点位置になる場所を求めます
+function originInCanvas2d(w, h, vec)
+{
+  var min_x = 0;
+  var max_x = 0;
+  var min_y = 0;
+  var max_y = 0;
+
+  for (var i=0 ; i<vec.length ; ++i) {
+    if (vec[i].x > max_x) {
+      max_x = vec[i].x;
+    }
+    if (vec[i].x < min_x) {
+      min_x = vec[i].x;
+    }
+
+    if (vec[i].y > max_y) {
+      max_y = vec[i].y;
+    }
+    if (vec[i].y < min_y) {
+      min_y = vec[i].y;
+    }
+  }
+
+  var scale = w / (max_x - min_x);
+
+  return new Position(Math.round(scale * -min_x), Math.round(scale * -min_y));
+}
+
+//J 領域サイズを指定するための構造体的なもの
+RectSize = function(w, h) {
+  this.w = w;
+  this.h = h;
+}
+
+//-----------------------------------------------------------------------------
+// 選択されているセンサデータのみグラフとして描画する
+//
+function drawLabeledLineGraph(canvas, title, data, min_val, max_val, xaxis_sub_ruled_interval, yaxis_sub_ruled_interval, col, sha, label)
+{
+  var ctx2d = canvas.getContext("2d");
+
+  var height = 500;
+  var width = data[0].length ;
+
+  var margin_left   = 50;
+  var margin_right  = 10;
+  var margin_top    = 5;
+  var margin_bottom = 5;
+
+  //J widthはデータ数に依存、heightはデータ種別数に依存
+  canvas.setAttribute("width",  (width  + margin_left + margin_right ).toString(10));
+  canvas.setAttribute("height", (height + margin_top  + margin_bottom).toString(10));
+
+  //J 下地は透明色にする
+  ctx2d.fillStyle = "rgba(0, 0, 0, 1)";
+  ctx2d.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+
+  _drawLineGraph(
+    ctx2d, 
+    data[0], 
+    margin_left,
+    margin_top,
+    width,
+    height,
+    xaxis_sub_ruled_interval,
+    yaxis_sub_ruled_interval,
+    min_val,
+    max_val,
+    title,
+    col[0],
+    sha[0],
+    "white"
+    );
+
+  for (let i=1 ; i<data.length ; ++i) {
+    _drawLineGraphBody(
+      ctx2d, 
+      data[i], 
+      margin_left,
+      margin_top,
+      width,
+      height,
+      min_val,
+      max_val,
+      col[i],
+      sha[i],
+      );
+   }
+
+  //J 各データのラベルを出す
+  let line_h = 10
+  let label_h = label.length * (line_h + margin_top) + margin_bottom
+  let label_w = 0;
+
+  for (let i=0 ; i<label.length ; ++i) {
+    if (label_w < ctx2d.measureText(label[i]).width) {
+      label_w = ctx2d.measureText(label[i]).width
+    }
+  }
+
+  let bar_w = 20
+  let margin_w = 5
+  label_w = margin_w + bar_w + margin_w + label_w + margin_w
+
+  let label_x = canvas.clientWidth - label_w - 3*margin_w
+  let label_y = margin_bottom + 2.5 * margin_top
+
+  ctx2d.fillStyle='black';
+  ctx2d.fillRect(label_x, 2 * margin_top, label_w, label_h);
+
+  ctx2d.beginPath();
+  ctx2d.strokeStyle='white';
+  ctx2d.lineWidth = 1;
+  ctx2d.strokeRect(canvas.clientWidth - label_w - 3*margin_w, 2 * margin_top, label_w, label_h);
+  ctx2d.stroke()
+
+  ctx2d.fillStyle='white';
+  for (let i=0 ; i<label.length ; ++i) {
+    ctx2d.beginPath();
+    ctx2d.strokeStyle=col[i];
+    ctx2d.moveTo(label_x + margin_w,         i*(line_h + margin_top) + label_y)
+    ctx2d.lineTo(label_x + margin_w + bar_w, i*(line_h + margin_top) + label_y)
+    ctx2d.stroke()
+
+    ctx2d.fillText(label[i], label_x + margin_w + bar_w + margin_w, i*(line_h + margin_top) + label_y + 3)
+
+    console.log(label[i], col[i])
+
+  }
+
+  return
+}
+
+//
+// RAWセンサデータの再描画
+//
+function updateRawDataGraph(raw_data_canvas, info_canvas, data)
+{
+// drawRawDataGraph(raw_data_canvas, data, 52*5, TrainigSensorName, TrainingSensorUnit);
+  drawInitInfoLayerOfRawDataGraph(raw_data_canvas, info_canvas);
+}
+
+//
+// Info Layerのサイズをグラフサイズに追従する
+//
+function drawInitInfoLayerOfRawDataGraph(raw_data_canvas, info_canvas)
+{
+  // Info Canvasのサイズを追従させる
+  var w = raw_data_canvas.getAttribute("width");
+  var h = raw_data_canvas.getAttribute("height");
+  info_canvas.setAttribute("width", w);
+  info_canvas.setAttribute("height", h);
+}
+
+
+//-----------------------------------------------------------------------------
+//J 比較用ラインの描画
+//
+function drawHighLightLine(canvas, x)
+{
+  var h = canvas.getAttribute("height");
+  var w = canvas.getAttribute("width");
+
+  var margin_left   = 50;
+  var margin_right  = 10;
+
+  if (margin_left < x && x < (w - margin_right)) {
+    _drawLine(canvas, x, 0, 0, h, "white");
+  }
+}
+
+//
+//J Raw Sensor Graphの座標からデータのIndexを逆参照する
+//
+function drawGetDataIndex(x, w)
+{
+  var margin_left 　= 50;
+  var margin_right = 10;
+
+  if (!(margin_left < x && x < (w - margin_right))) {
+    return -1;
+  }
+
+  var data_idx = x - margin_left;
+
+  return data_idx
+}
+
+//
+//J Graphに補助線とLabelを描画
+//
+function drawDataPointerAndLabel(canvas, data, selecter, x)
+{
+  var height = 150;
+  var margin_top = 5;
+  var margin_bottom = 5;
+  var w = canvas.getAttribute("width");
+
+  var data_idx = drawGetDataIndex(x, w);
+  if (data_idx < 0) return;
+
+  var j = 0;
+  var max_velocity = Math.max(Math.max(...data[1]), Math.max(...data[2]), Math.max(...data[3]))
+
+  for (var i=0 ; i<4 ; ++i) {
+    if (i == 0){
+      var scale = height / (Math.max(...data[i]) - 0);
+      var y = j*(height + margin_top + margin_bottom) + height - ((data[i][data_idx] - 0) * scale);
+      _drawPointer(canvas, x, y, 5);
+      _drawLabel(canvas, x+5, y, data[i][data_idx].toString(), "white")
+      j++;
+    }
+    else {
+      var scale = height / (max_velocity - 0)
+      var y = j*(height + margin_top + margin_bottom) + height - ((data[i][data_idx] - 0) * scale);
+      _drawPointer(canvas, x, y, 5);
+      _drawLabel(canvas, x+5, y, data[i][data_idx].toString(), "white")
+    }
+  }
+  j++;
+
+  for (var i=0 ; i<9 ; ++i) {
+    if (selecter.sensors[i].checked) {
+      var scale = height / (Math.max(...data[i+4]) - Math.min(...data[i+4]))
+      var y = j*(height + margin_top + margin_bottom) + height - ((data[i+4][data_idx] - Math.min(...data[i+4])) * scale);
+      _drawPointer(canvas, x, y, 5);
+      _drawLabel(canvas, x+5, y, data[i+4][data_idx].toString(), "white")
+      j++;
+    }
+  }
+
+}
+
+//
+//J 時間間隔を表示する
+//
+function drawTimeCursor(canvas, p1, p2, scale)
+{
+  var delta = Math.abs(p1-p2);
+  var start = (p1 > p2) ? p2 : p1;
+
+  _drawLine(canvas, start, 50, delta, 0, "white");
+
+  _drawLine(canvas, start, 50, 5, 5, "white");
+  _drawLine(canvas, start, 50, 5,-5, "white");
+
+  _drawLine(canvas, start + delta, 50, -5, 5, "white");
+  _drawLine(canvas, start + delta, 50, -5,-5, "white");
+
+  var delta_time = Math.round((delta * scale) * Math.pow(10, 4) ) / Math.pow(10, 4); 
+
+  _drawLabel(canvas, start + delta + 20, 50, delta_time.toString(), "white")
+}
+
+//-----------------------------------------------------------------------------
+//J 対象のCanvasの内容をすべて消去
+//
+function drawClearAll(canvas)
+{
+  _drawClearAll(canvas);
+}
+
---
+++ src/tools/data_view_ctrl.js
@@ -0,0 +1,201 @@
+﻿/*
+ * data_view_ctrl.js
+ *
+ * Created: 2022/08/20
+ * Copyright 2022 Kiyotaka Akasaka. All rights reserved.
+ */
+/*-------------------------------------------------------------------------------------------------
+ * 各種変数/定数定義
+ */
+var DATA_SET={}
+
+
+/*-----------------------------------------------------------------------------
+ * HTML Form まわりの挙動を記載
+ */
+window.onload = function() {
+}
+
+//J Re-size時には描画をやり直す
+window.onresize = function () {
+}
+
+
+
+//-----------------------------------------------------------------------------
+// HTML UI Callbacks
+
+//
+//J Learning / Mapボタンが押されたときの挙動
+//
+function onFileLoadButtonClick()
+{
+  document.getElementById("data_file").click();
+  document.getElementById("data_file").addEventListener('change', handleDataFileSelect, false);
+}
+
+
+//-----------------------------------------------------------------------------
+//J File Selecterがクリックされたときの挙動
+//
+function handleDataFileSelect(evt)
+{
+  var file = evt.target.files[0];
+
+  //J ファイル名の表示
+  data_file.innerHTML = file.name;
+
+  //J ファイルの内容をパースする
+  loadAndParseDataFile(file);
+}
+
+//
+//J Map Dataの読み込み処理
+//
+function loadAndParseDataFile(file)
+{
+  var reader = new FileReader();
+
+  console.log(file.name)
+
+  document.getElementById('data_file_name').innerHTML = file.name
+
+  //J ファイルの中身を読み込む
+  reader.onload = function (evt) {
+    //J 1行に区切って処理する
+    var lines = reader.result.split(/\r\n|\r|\n/);
+    var latest = ''
+    for (var i=0 ; i<lines.length ; ++i) {
+      if (lines[i].indexOf("#") == 0) {
+        continue; // skip
+      }
+      else if (lines[i].indexOf("$") == 0) {
+	token = lines[i].substr(1).trim().split(/\t/)
+        DATA_SET[token[0]] = new Array(token.length-1)
+        latest = token[0]
+        for (let y = 1; y < token.length ; ++y) {
+          DATA_SET[latest][y-1] = ([token[y],[]])
+        }
+      }
+      else if (latest != '') {
+        token = lines[i].trim().split(/\t/)
+        if (token.length != DATA_SET[latest].length) {
+          continue
+        }
+        for (let y = 0; y < token.length ; ++y) {
+          DATA_SET[latest][y][1].push(token[y])
+        }
+      }
+    }
+
+    updateDataset(DATA_SET)
+
+    return;
+  }
+
+  reader.readAsText(file);
+}
+
+function updateDataset(dataset)
+{
+  html = ''
+
+  var i=0;
+  var init_key = '';
+  for (let key in dataset) {
+    if (i==0) { html += '<input class="data_list_sel" type="radio" name="data_set" onchange="changeDataset(value, DATA_SET)" value="'+ key +'" checked>' + key; init_key = key; }
+    else      { html += '<input class="data_list_sel" type="radio" name="data_set" onchange="changeDataset(value, DATA_SET)" value="'+ key +'">' + key }
+
+    i++;
+  }
+
+  document.getElementById('data_list').innerHTML = html
+  changeDataset(init_key, DATA_SET)
+
+  return;
+}
+
+var CURRENT_SET=''
+var CURRENT_ITEM=''
+function changeDataset(set, dataset)
+{
+  CURRENT_SET = set;
+  var html=''
+
+  html+= '<select name="data_item_select" onchange="changeDataitem(value, CURRENT_SET, DATA_SET)">'
+  html+= '<option value="all">all</option>'
+  for (let i=0 ; i<dataset[set].length ; ++i) {
+    html += '<option value="'+ dataset[set][i][0] +'">' + dataset[set][i][0] + '</option>'
+  }
+  html += '</select>'
+
+  let min_arr = []
+  let max_arr = []
+  for (let i=0 ; i<dataset[set].length ; ++i) {
+    min_arr.push(Math.min(...dataset[set][i][1]))
+    max_arr.push(Math.max(...dataset[set][i][1]))
+  }
+
+  html += '<div> <input type="range" id="range_min" name="range_min" onchange="changeDataitem(CURRENT_ITEM, CURRENT_SET, DATA_SET)" value="'+ Math.round(Math.min(...min_arr)).toString() +'" min="'+ Math.round(Math.min(...min_arr)).toString() +'" max="'+ Math.round(Math.max(...max_arr)).toString() +'"><label id="label_min" for="range_min">Min</label></div>'
+  html += '<div> <input type="range" id="range_max" name="range_max" onchange="changeDataitem(CURRENT_ITEM, CURRENT_SET, DATA_SET)" value="'+ Math.round(Math.max(...max_arr)).toString() +'" min="'+ Math.round(Math.min(...min_arr)).toString() +'" max="'+ Math.round(Math.max(...max_arr)).toString() +'"><label id="label_max"for="range_max">Max</label></div>'
+
+  document.getElementById('data_item').innerHTML = html
+
+  changeDataitem('all',set, dataset)
+}
+
+function changeDataitem(item, set, dataset)
+{
+  CURRENT_ITEM = item
+
+  var canvas = document.getElementById('main_graph')
+  dat = []
+  let COL = ['#f5deb3', '#ffff00', '#ffd700', '#ffa500', '#f4a460', '#ff8c00', '#daa520', '#cd853f', '#b8860b', '#d2691e', '#a0522d']
+  let SHA = ['#f0e68c', '#f0e68c', '#f0e68c', '#f0e68c', '#f0e68c', '#f0e68c', '#f0e68c', '#f0e68c', '#f0e68c', '#f0e68c', '#f0e68c']
+
+  let col = []
+  let sha = []
+  let label = []
+
+  let rsvd = -1
+  for (let i=0 ; i<dataset[set].length ; ++i) {
+    if (item == dataset[set][i][0]) {
+      rsvd = i;
+    }
+    else {
+      label.push(dataset[set][i][0])
+      dat.push(dataset[set][i][1])
+      col.push(COL[i])
+      sha.push(SHA[i])
+    }
+  }
+
+  if (rsvd != -1) {
+    label.push(dataset[set][rsvd][0])
+    dat.push(dataset[set][rsvd][1])
+
+    for (let i=0 ; i<col.length ; ++i) {
+      col[i] = '#696969'
+      sha[i] = '#a9a9a9'
+    }
+
+    col.push(COL[rsvd])
+    sha.push(SHA[rsvd])
+  }
+
+  let range_max = parseInt(document.getElementById('range_max').value)
+  let range_min = parseInt(document.getElementById('range_min').value)
+  sub_y = Math.round((range_max - range_min) / 5)
+  if (sub_y == 0) sub_y=1
+
+  document.getElementById('label_min').innerHTML = 'MIN '+range_min.toString()
+  document.getElementById('label_max').innerHTML = 'MAX '+range_max.toString()
+
+
+  console.log(range_min, range_max,sub_y)
+
+  drawLabeledLineGraph(canvas, set, dat, range_min, range_max, 52, sub_y, col, sha, label)
+
+
+  return;
+}
